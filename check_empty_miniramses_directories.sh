afc2bf1176daa8670d21a19c633b6392a550884b
Luke Conaboy <lukeconaboy@gmail.com>
2024-07-13 20:08:31 +0200

    updated utils

---
+++ check_empty_miniramses_directories.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+x=(backup_00*)
+
+min=-1
+max=$(( ${#x[@]} -1 ))
+
+for ((i=$max; i>$min; i--)); do
+    n=$(ls ${x[$i]} | wc -l);
+    if [[ $n -gt 0 ]]; then
+	echo ${x[$i]} full;
+    else
+	echo ${x[$i]} empty;
+    fi
+done
---
+++ update_miniramses_nrestart.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+
+# Run from the directory containing the ramses output dirs. Uses a few
+# other shell scripts to find the latest non-empty output dir and
+# extracts the output number, then inserts into the namelist file,
+# making a backup of the old one. Works in the case of empty/nonempty
+# output dirs (i.e. running with withoutmkdir=.true.)
+
+lo=$(. find_latest_miniramses_output.sh)  # latest output_xxxxx
+
+if [[ $lo =~ _([0-9]{5}) ]]; then
+    printf -v ln "%g" ${BASH_REMATCH[1]}  # latest output n
+else
+    ln=0  # no outputs found
+fi
+
+nml=($(ls *.nml))
+
+if [[ ${#nml[@]} -ne 1 ]]; then
+    echo More than one namelist found, exiting
+    exit 1
+fi
+
+echo Updating nrestart to $ln
+cp $nml $nml.bak
+sed -i "/nrestart/ c nrestart=$ln" $nml
